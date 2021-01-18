72289de556e336d0b8d26f34b031f4b907866598
Rodolfoxbc <rodolfo.bojorque@gmail.com>
2021-01-18 13:00:40 -0500

    Create NormalEquation.m

---
+++ NormalEquation.m
@@ -0,0 +1,19 @@
+#Cargar el dataset
+data=dlmread("data.csv",";");
+
+#Extract features (X) and labels (Y)
+#we suppose that labels are the last column
+X=data(:,1:size(data,2)-1);
+y=data(:,size(data,2));
+
+#Add interception term
+X=[ones(size(X,1),1) X];
+
+#Apply normal equation to find out tetha values
+theta=pinv(X'*X)*(X'*y)
+
+#With theta values we need calculate hypothesis
+h=X*theta;
+
+#Determinate cost function (J)
+J=(sum((h-y).^2))^(1/2)
